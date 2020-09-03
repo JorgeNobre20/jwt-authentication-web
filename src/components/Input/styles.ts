a972f838d02d60bdf8c70a2529385c7b21c07dcc
Jorge Nobre <jorgenobre1234@gmail.com>
2020-09-03 09:52:59 -0300

    update input component style and status

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -14,10 +14,17 @@ export const InputContainer = styled.input`
     padding: 0px 10px;
     margin-bottom: 1.5rem;
     font-size: 0.9rem;
+    transition: 0.4s;
+
 
     ::placeholder{
         color: var(--placeholder-text-color);
         font-weight: 400;
         font-size: 0.9rem;
+    }
+
+    &:focus,
+    &:valid{
+        border-color: var(--secondary-color);
     }
 `;
