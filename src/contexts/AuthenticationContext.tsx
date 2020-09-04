c9d7aaba93b3397bb66976b3666c58d2e0f7e8a7
Jorge Nobre <jorgenobre1234@gmail.com>
2020-09-04 12:34:11 -0300

    add auth context structure

---
+++ src/contexts/AuthenticationContext.tsx
@@ -0,0 +1,40 @@
+import React, { useContext, createContext, useState, useEffect } from "react";
+
+interface IUser{
+    id: number; 
+    username: string;
+}
+
+interface IAuthContextData{
+    user: IUser | null;
+    signed: boolean;
+    signOut: () => void;
+    signIn: () => void;
+}
+
+const AuthContext = createContext<IAuthContextData>({} as IAuthContextData);
+
+const AuthProvider: React.FC = ({ children }) => {
+
+    const [user,setUser] = useState<IUser | null>(null);
+
+    function signOut(){};
+    function signIn(){};
+
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ signed: !!user , user: { username: "", id: 5 }, signIn, signOut }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export const userAuth = (): IAuthContextData => {
+    const context = useContext(AuthContext);
+    return context;
+}
+
+export default AuthProvider;
